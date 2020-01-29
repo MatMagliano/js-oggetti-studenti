9932667d84d25314f8355ecfe7226939b1411efc
Matteo Magliano <matteo.magliano01@gmail.com>
2020-01-29 16:17:30 +0100

    stampa a schermo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  alert('Hello');
+  //alert('Hello');
+
+  //Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+  var student = {
+    name: 'Matteo',
+    surname: 'Magliano',
+    age: '37'
+  };
+  for (var key in student) {
+    console.log(key + ': ' + student[key]);
+    $('#student').append(key + ': ' + student[key] + ' / ');
+
+  }
 });
